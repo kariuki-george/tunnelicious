33abef010932e96eb5a2e025fb214946fc3f89f2
George Thuo Kariuki <kariukigeorge2030@gmail.com>
2025-12-09 11:57:57 +0300

    fix: update packages for security reasons

--- ctrl/lib/auth-client.ts
+++ ctrl/lib/auth-client.ts
@@ -1,12 +1,8 @@
-import {  passkeyClient } from "better-auth/client/plugins";
+// import { passkeyClient } from "@better-auth/passkey/client";
 import { createAuthClient } from "better-auth/react";
 
-
 const auth = createAuthClient({
-  plugins: [
-    passkeyClient(),
-   
-  ],
+  // plugins: [passkeyClient()],
 });
 
 export const { signIn, signOut, useSession } = auth;
